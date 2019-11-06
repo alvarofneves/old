d1062c621ad8fe887adb023517d62db2e621eea9
David <david.pp1997@gmail.com>
2019-11-06 00:19:35 +0000

    //Added Project

---
+++ resources/js/components/userList.vue
@@ -0,0 +1,51 @@
+<template>
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Age</th>
+                <th>Department</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="user in users" :key="user.id" :class="{active: currentUser === user}">
+                <td>{{ user.name }}</td>
+                <td>{{ user.email }}</td>
+                <td>{{ user.age }}</td>
+                <td>{{ user.department }}</td>
+                <td>
+                    <a class="btn btn-sm btn-primary" v-on:click.prevent="editUser(user)">Edit</a>
+                    <a class="btn btn-sm btn-danger" v-on:click.prevent="deleteUser(user)">Delete</a>
+                    <a class="btn btn-sm btn-success" v-on:click.prevent="definePlayer(user, 1)">P1</a>
+                    <a class="btn btn-sm btn-success" v-on:click.prevent="definePlayer(user, 2)">P2</a>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<script>
+    export default {
+        props: ['users','currentUser'],
+        data: function () {
+            return {
+            }
+        },
+        methods: {
+            editUser(user) {
+                this.$emit('edit-user',user);
+            },
+            deleteUser(user) {
+                this.$emit('delete-user',user);
+            },
+            definePlayer(user, playerNumber){
+                this.$root['player' + playerNumber];
+            }
+        }
+    }
+</script>
+<style>
+
+</style>
---
+++ resources/js/vue.js
@@ -0,0 +1,43 @@
+/*jshint esversion: 6 */
+
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
+require('./bootstrap');
+
+window.Vue = require('vue');
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
+
+import Users from './components/users';
+import Departments from './components/departments';
+import Game from './components/game';
+
+const routes = [
+    {path:'/', redirect:'/users'},
+    {path:'/users', component:Users},
+    {path:'/departments', component:Departments},
+    {path:'/tictactoe', component:Game}
+]
+
+const router = new VueRouter({
+    routes
+})
+
+const app = new Vue({
+    el: '#app',
+    router,
+    data: {
+        
+    },
+    methods: {
+        
+    },
+    mounted() {
+
+    }
+    
+});
